712dfd187edf3c0003630ca58f317f30bfb89f69
Jia Hao <>
2023-05-30 00:46:56 +0800

    added favicon

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -7,6 +7,13 @@ import BettingComponent from "../components/BettingComponent";
 const Home: NextPage = () => {
   return (
     <main className={styles.main}>
+      <Head>
+        <title>Head Tail Game</title>
+        <meta name="description" content="Head Tail Game" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
+      </Head>
       <BettingComponent />
     </main>
   );
