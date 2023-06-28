2ab00ce9106f11b0bfba2322e8c8135f6f251173
lavatalis <135064792+lavatalis@users.noreply.github.com>
2023-06-28 21:53:32 +0000

    Pending changes exported from your codespace

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  distDir: "build",
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
